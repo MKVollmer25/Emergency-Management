e769fbf481de511ca593ac2bc6c5395ab9f82247
Michael Vollmer <michaelkvollmer@gmail.com>
2025-07-09 17:49:07 -0700

    Major user and admin dashboard updates

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,8 +1,111 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWater } from '@fortawesome/free-solid-svg-icons';
+import { faFire } from '@fortawesome/free-solid-svg-icons';
+import { faDroplet } from '@fortawesome/free-solid-svg-icons';
+import { faToilet } from '@fortawesome/free-solid-svg-icons';
+import { faHouseChimneyCrack } from '@fortawesome/free-solid-svg-icons';
+import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
+
 function Admin() {
+  const date = new Date()
+
+  const date_options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
+
+  const time_options = {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true
+  }
+
+  const formatted_date = date.toLocaleDateString("en-US", date_options)
+  const formatted_time = date.toLocaleTimeString("en-US", time_options)
+
   return (
-    <section>
+    <>
+      <header className="bg-blue-900 text-white shadow-md px-6 py-4 md:py-6">
         WIP
-    </section>
+      </header>
+      <div className="flex justify-between px-4 py-4">
+        <div>{formatted_date}, {formatted_time}</div>
+        <div>BUTTONS</div>
+      </div>
+      <main className="px-4 pb-16">
+        <div>
+          <div className="bg-blue-100 border-l-4 border-blue-500 text-blue-900 p-4 rounded mb-6">
+            <p className="text-lg font-semibold">Weather in San Francisco:</p>
+            <p>Temperature: WIP</p>
+            <p>Conditions: WIP</p>
+          </div>
+        </div>
+        <div className="grid grid-cols-3 grid-rows-2 gap-6">
+          <a class="bg-white border border-gray-200 rounded-xl shadow-md p-6 hover:shadow-lg hover:border-blue-500 transition-all duration-300 group">
+            <div class="mb-2 text-center">
+              <FontAwesomeIcon icon={faWater} size="2x" color="#999999" />
+              <h2 class="text-lg font-semibold text-gray-800 group-hover:text-blue-600">Flood Dashboard</h2>
+              <p class="text-sm text-gray-600">Live flood incident tracking</p>
+            </div>
+            <div class="mt-2 text-right">
+              <span class="text-sm text-blue-500 font-medium group-hover:underline">View →</span>
+            </div>
+          </a>
+          <a class="bg-white border border-gray-200 rounded-xl shadow-md p-6 hover:shadow-lg hover:border-blue-500 transition-all duration-300 group">
+            <div class="mb-2 text-center">
+              <FontAwesomeIcon icon={faFire} size="2x" color="#FF7700" />
+              <h2 class="text-lg font-semibold text-gray-800 group-hover:text-blue-600">Fire Dashboard</h2>
+              <p class="text-sm text-gray-600">Live fire incident tracking</p>
+            </div>
+            <div class="mt-2 text-right">
+              <span class="text-sm text-blue-500 font-medium group-hover:underline">View →</span>
+            </div>
+          </a>
+          <a class="bg-white border border-gray-200 rounded-xl shadow-md p-6 hover:shadow-lg hover:border-blue-500 transition-all duration-300 group">
+            <div class="mb-2 text-center">
+              <FontAwesomeIcon icon={faDroplet} size="2x" color="#0000FF" />
+              <h2 class="text-lg font-semibold text-gray-800 group-hover:text-blue-600">Water Dashboard</h2>
+              <p class="text-sm text-gray-600">Live water incident tracking</p>
+            </div>
+            <div class="mt-2 text-right">
+              <span class="text-sm text-blue-500 font-medium group-hover:underline">View →</span>
+            </div>
+          </a>
+          <a class="bg-white border border-gray-200 rounded-xl shadow-md p-6 hover:shadow-lg hover:border-blue-500 transition-all duration-300 group">
+            <div class="mb-2 text-center">
+              <FontAwesomeIcon icon={faToilet} size="2x" color="#993333" />
+              <h2 class="text-lg font-semibold text-gray-800 group-hover:text-blue-600">Sewer Dashboard</h2>
+              <p class="text-sm text-gray-600">Live sewer incident tracking</p>
+            </div>
+            <div class="mt-2 text-right">
+              <span class="text-sm text-blue-500 font-medium group-hover:underline">View →</span>
+            </div>
+          </a>
+          <a class="bg-white border border-gray-200 rounded-xl shadow-md p-6 hover:shadow-lg hover:border-blue-500 transition-all duration-300 group">
+            <div class="mb-2 text-center">
+              <FontAwesomeIcon icon={faHouseChimneyCrack} size="2x" color="#000000" />
+              <h2 class="text-lg font-semibold text-gray-800 group-hover:text-blue-600">Earthquake Dashboard</h2>
+              <p class="text-sm text-gray-600">Live earthquake incident tracking</p>
+            </div>
+            <div class="mt-2 text-right">
+              <span class="text-sm text-blue-500 font-medium group-hover:underline">View →</span>
+            </div>
+          </a>
+          <a class="bg-white border border-gray-200 rounded-xl shadow-md p-6 hover:shadow-lg hover:border-blue-500 transition-all duration-300 group">
+            <div class="mb-2 text-center">
+              <FontAwesomeIcon icon={faCircleQuestion} size="2x" color="#555555" />
+              <h2 class="text-lg font-semibold text-gray-800 group-hover:text-blue-600">Miscellaneous Dashboard</h2>
+              <p class="text-sm text-gray-600">Live miscellaneous incident tracking</p>
+            </div>
+            <div class="mt-2 text-right">
+              <span class="text-sm text-blue-500 font-medium group-hover:underline">View →</span>
+            </div>
+          </a>
+        </div>
+      </main>
+    </>
   )
 }
 
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,54 @@
 function Dashboard() {
+  const date = new Date()
+
+  const date_options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
+
+  const time_options = {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true
+  }
+
+  const formatted_date = date.toLocaleDateString("en-US", date_options)
+  const formatted_time = date.toLocaleTimeString("en-US", time_options)
+
   return (
-    <header class="bg-blue-900 text-white shadow-md px-6 py-4 md:py-6">Top of page</header>
+    <>
+      <header className="bg-blue-900 text-white shadow-md px-6 py-4 md:py-6">
+        WIP
+      </header>
+      <div className="flex justify-between px-4 py-4">
+        <div>{formatted_date}, {formatted_time}</div>
+        <div>BUTTONS</div>
+      </div>
+      <main className="px-4 pb-16">
+        <div>
+          <div className="bg-blue-100 border-l-4 border-blue-500 text-blue-900 p-4 rounded mb-6">
+            <p className="text-lg font-semibold">Weather in San Francisco:</p>
+            <p>Temperature: WIP</p>
+            <p>Conditions: WIP</p>
+          </div>
+        </div>
+        <div class="bg-white p-6 rounded-lg shadow mb-6">
+          <p class="text-lg font-semibold text-gray-700">Total Complaints Filed: WIP</p>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+          <a class="bg-white border-t-4 border-blue-600 rounded-2xl p-6 shadow hover:bg-blue-50">
+            <h3 class="text-lg font-bold text-blue-700 mb-2">File a New Complaint</h3>
+            <p class="text-gray-600">Report emergencies quickly and easily.</p>
+          </a>
+          <a class="bg-white border-t-4 border-green-600 rounded-2xl p-6 shadow hover:bg-green-50">
+            <h3 class="text-lg font-bold text-green-700 mb-2">View Your Complaints</h3>
+            <p class="text-gray-600">Track the status of your past complaints and get updates.</p>
+          </a>
+        </div>
+      </main>
+    </>
   )
 }
 
