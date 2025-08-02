0522c3cdc0be4c0811d4fc7335ba146d43012354
Michael Vollmer <michaelkvollmer@gmail.com>
2025-08-02 00:37:44 -0700

    Map icon fix

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -18,6 +18,20 @@ import {
 } from 'recharts';
 import API from '../components/API';
 
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+});
+
 function capitalizeWords(str) {
   return str
     .split(' ')
